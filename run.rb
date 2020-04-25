e041010ac30ec69591cfa8ca5fa92be09377c0d9
CharliePayne1 <charliepayne1101@gmail.com>
2020-04-25 15:48:36 +0100

    Done.

--- run.rb
+++ run.rb
@@ -4,6 +4,14 @@ require 'pry'
 
 
 #Test your code here
+london = Zoo.new("London Zoo", "UK")
+sydney = Zoo.new("Sydney Zoo", "Aus")
+shanghai = Zoo.new("Shanghai Zoo", "China")
+
+alex = Animal.new("lion", 120, "Alex", london)
+gloria = Animal.new("hippo", 180, "Gloria", sydney)
+melman = Animal.new("giraffe", 100, "Melman", shanghai)
+sid = Animal.new("zebra", 70, "Sid", london)
 
 
 binding.pry
